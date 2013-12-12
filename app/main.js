e4762025599da7c6b8c304dc0d94c1992c3541e4
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2013-12-12 10:25:52 +0800

    first run version

---
+++ app/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
+ */
+chrome.app.runtime.onLaunched.addListener(
+    function () {
+        chrome.app.window.create('index.html', {//http://developer.chrome.com/apps/app_window.html#method-create
+                id: 'main',
+                'bounds': { //http://developer.chrome.com/apps/app_window.html#type-Bounds
+                    'top': 10,
+                    'left': 10
+                }
+            }
+        );
+    }
+)
