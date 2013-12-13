1b24f83ed33ee14c122d68267c5499ae43201233
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2013-12-13 17:43:12 +0800

    change project structure

--- chrome_app/background.js
+++ chrome_app/background.js
@@ -3,7 +3,7 @@
  */
 chrome.app.runtime.onLaunched.addListener(
     function () {
-        chrome.app.window.create('index.html', {//http://developer.chrome.com/apps/app_window.html#method-create
+        chrome.app.window.create('chromeindex.html', {//http://developer.chrome.com/apps/app_window.html#method-create
                 id: 'main',
                 'bounds': { //http://developer.chrome.com/apps/app_window.html#type-Bounds
                     'top': 10,
